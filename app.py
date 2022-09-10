b5b0971aad18df3e48100629a9af8727d7a650be
Hjayej Mohamed <mohamedhjaij800@gmail.com>
2022-09-10 19:17:44 +0200

    Version 3

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 app=Flask(__name__)
 #load the model
 model=pickle.load(open('VotingModel.pkl','rb'))
+scaler=pickle.load(open('scaler.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -14,12 +15,10 @@ def home():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
-    print(data)
-    scalar=pickle.load(open('scaler.pkl','rb'))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
+    return (jsonify(int(output[0])))
+
 
 if __name__=="__main__":
     app.run(debug=True)

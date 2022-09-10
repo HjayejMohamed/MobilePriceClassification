0593509150a2ed28bc87d7ae615f36f88c577678
Hjayej Mohamed <mohamedhjaij800@gmail.com>
2022-09-11 00:55:29 +0200

    Version 8

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
     final_input=scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text='the house price prediction is {}'.format(output))
+    return render_template("home.html",prediction_text='the house price classe is {}'.format(output))
 
 
 if __name__=="__main__":
